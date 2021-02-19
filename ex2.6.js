b9527d685d382298c5d6fc745abfdfb0576eebaa
ordegani <77922002+ordegani@users.noreply.github.com>
2021-02-19 17:01:00 +0200

    Add files via upload

---
+++ ex2.6.js
@@ -0,0 +1,12 @@
+function century(year) {
+    let result = 0;
+    for (let i = 0; i < year; i++) {
+      if (i % 100 === 0) {
+        result++;
+      }
+    }
+    return result;
+  }
+  //check
+  year = 1778;
+  console.log(century(year))
